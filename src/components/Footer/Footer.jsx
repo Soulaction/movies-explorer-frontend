8e2539739116aaf676fc3e6bb118ba8058c81c1e
Dmitry <ds-8.5@bk.ru>
2023-09-23 11:06:10 +0300

    feat: свёрстан черновой вариант Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,20 @@
+import './Footer.css'
+
 const Footer = () => {
-    return (<div></div>)
+    return (
+        <footer className="footer">
+            <div className="footer__content">
+                <p className="footer__name">Учебный проект Яндекс.Практикум х BeatFilm.</p>
+                <div className="footer__wrapper">
+                    <p className="footer__year">&copy; 2023</p>
+                    <nav className="footer__nav">
+                        <a className="footer__link" href="https://practicum.yandex.ru" target="_blank">Яндекс.Практикум</a>
+                        <a className="footer__link" href="https://github.com/Soulaction?tab=projects" target="_blank">Github</a>
+                    </nav>
+                </div>
+            </div>
+        </footer>
+    )
 }
+
+export default Footer;
