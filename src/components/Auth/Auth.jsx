463908d1c3618b7e7ad4976773802a73184312e0
Dmitry <ds-8.5@bk.ru>
2023-09-27 19:53:07 +0300

    feat: допилил адаптив для странцы "Автоизации/Регистрации"

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -8,13 +8,16 @@ const Auth = ({title, isLoginPage}) => {
             <form className="auth__form" name="auth-user" noValidate>
                 <img className="auth__logo" src={logo}/>
                 <h1 className="auth__title">{title}</h1>
-                <label className={`auth__label${isLoginPage ? ' auth__label_none' : ''}`} htmlFor="input-name">Имя</label>
+                <label className={`auth__label${isLoginPage ? ' auth__label_none' : ''}`}
+                       htmlFor="input-name">Имя</label>
                 <input id="input-name"
                        className={`auth__input${isLoginPage ? ' auth__input_none' : ''}`}
                        type="text"
                        name="name"
                        placeholder="Введите имя"/>
-                <span className="auth__text-error auth__text-error_hidden">Что-то пошло не так...</span>
+                <span className={`auth__text-error${isLoginPage ? ' auth__text-error_none' : ''}`}>
+                    Что-то пошло не так...
+                </span>
                 <label className="auth__label" htmlFor="input-email">E-mail</label>
                 <input id="input-email"
                        className="auth__input auth__input_error"
@@ -29,10 +32,11 @@ const Auth = ({title, isLoginPage}) => {
                        name="password"
                        placeholder="Введите пароль"/>
                 <span className="auth__text-error">Что-то пошло не так...</span>
-                <button className={`auth__btn${isLoginPage ? ' auth__btn_login' : ''}`}>Зарегестрироваться</button>
+                <button className={`auth__btn${isLoginPage ? ' auth__btn_login' : ''}`}>{isLoginPage ? 'Войти' : 'Зарегестрироваться'}</button>
                 <p className="auth__text">
-                    {isLoginPage ? 'Уже зарегестрированы? ' : 'Ещё не зарегестрированы? '}
-                    <NavLink className="auth__link" to={isLoginPage ? '/signup' : '/signin'}>{isLoginPage ? 'Регистрация' : 'Войти'}</NavLink>
+                    {isLoginPage ? 'Ещё не зарегестрированы? ' : 'Уже зарегестрированы? '}
+                    <NavLink className="auth__link"
+                             to={isLoginPage ? '/signup' : '/signin'}>{isLoginPage ? 'Регистрация' : 'Войти'}</NavLink>
                 </p>
             </form>
         </sectinon>
